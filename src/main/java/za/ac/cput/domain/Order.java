7ed439ac655efae008e0d389d24466b152f0020e
MNISIV <222929456@mycput.ac.za>
2026-03-23 18:57:35 +0200

    I added two classes

---
+++ src/main/java/za/ac/cput/domain/Order.java
@@ -0,0 +1,9 @@
+package za.ac.cput.domain;
+/*
+AnimeStore.java
+Order class
+Author: Vumbhoni Clifford Mnisi (222929456)
+Date: 23 March 2026
+*/
+public class Order {
+}
---
+++ src/main/java/za/ac/cput/domain/OrderItem.java
@@ -0,0 +1,10 @@
+package za.ac.cput.domain;
+/*
+AnimeStore.java
+OrderItem class
+Author: Vumbhoni Clifford Mnisi (222929456)
+Date: 23 March 2026
+*/
+
+public class OrderItem {
+}
