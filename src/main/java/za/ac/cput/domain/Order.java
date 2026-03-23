bdc9763ec5b34b87e0ca6a3e4813fc15a5d6e00b
MNISIV <222929456@mycput.ac.za>
2026-03-23 19:08:09 +0200

    I added two classes

--- src/main/java/za/ac/cput/domain/Order.java
+++ src/main/java/za/ac/cput/domain/Order.java
@@ -6,4 +6,16 @@ Author: Vumbhoni Clifford Mnisi (222929456)
 Date: 23 March 2026
 */
 public class Order {
+    private String orderId;
+    private String itemDate;
+    private double amount;
+    private String status;
+
+    private Order(){
+
+    }
+
+    public Order(String orderId, String itemDate, double amount, String status) {
+        this.orderId = orderId;
+    }
 }
--- src/main/java/za/ac/cput/domain/OrderItem.java
+++ src/main/java/za/ac/cput/domain/OrderItem.java
@@ -7,4 +7,11 @@ Date: 23 March 2026
 */
 
 public class OrderItem {
+   private String orderItemId;
+   private int quantity;
+   private double unitPrice;
+
+   private OrderItem(){
+
+    }
 }
