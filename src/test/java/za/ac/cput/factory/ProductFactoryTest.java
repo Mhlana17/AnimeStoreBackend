55b7914c0607892ff4e07b4a18a8517a8c83b05f
SisonkeMhlana <mhlanasisonke17@gmail.com>
2026-03-24 17:09:51 +0200

    Update

--- src/test/java/za/ac/cput/factory/ProductFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ProductFactoryTest.java
@@ -1,22 +1,27 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.BeforeEach;
 import za.ac.cput.domain.Product;
 import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductFactoryTest {
-    private static Product product1 = ProductFactory.createProduct(23L,"hoody",499.00);
-    private static Product product2 = ProductFactory.createProduct(24L,"hat", 120.00);
+    private Product product1;
+
+    @BeforeEach
+    void setUp() {
+
+        product1 = ProductFactory.createProduct( 49L,"Socks",29.00);
+    }
 @Test
-    public void testCreateProduct(){
+     void testCreateProduct(){
         assertNotNull(product1);
         System.out.println(product1.toString());
     }
 
-    public void testCreateProduct1(){
-        assertNotNull(product2);
-        System.out.println(product2.toString());
-    }
+
 
 
 }//end of class
